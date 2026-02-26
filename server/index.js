738f2c63470e063734a4e50c5321b6f4cacbe1e2
khemistry <otisfrancis1@gmail.com>
2026-02-26 09:15:24 +0000

    updated cors origin to allow from all

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ import entryRoutes from './routes/entries.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 
 app.use('/api', entryRoutes);
